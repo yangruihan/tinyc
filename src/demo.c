f9ddff3ccc7c81dfe1986fb4104e667f50697f27
C.y <yangruihan@vip.qq.com>
2018-12-17 16:03:42 +0800

    add scanner

---
+++ src/demo.c
@@ -0,0 +1,13 @@
+int main()
+{
+    int a, b;
+    a = readint();
+    b = readint();
+    if (a > b) {
+        print("a>b %d", a);
+    } else {
+        print("a<=b %d", b);
+    }
+    return 0;
+}
+
