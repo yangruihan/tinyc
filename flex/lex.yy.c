b58500e53bf966af2a981138a0bb1662d2312fc9
C.y <yangruihan@vip.qq.com>
2018-12-17 15:34:45 +0800

    add word-spliter example

--- flex/lex.yy.c
+++ flex/lex.yy.c
@@ -360,19 +360,19 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[10] =
+static const flex_int16_t yy_accept[9] =
     {   0,
-        0,    0,    5,    3,    4,    2,    1,    1,    0
+        0,    0,    5,    2,    3,    1,    2,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
     {   0,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    2,
+        1,    1,    1,    1,    1,    1,    2,    1,    2,    3,
+        1,    1,    2,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    2,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    3,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    4,    4,    4,
-        4,    4,    4,    4,    4,    4,    4,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -397,31 +397,29 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static const YY_CHAR yy_meta[5] =
+static const YY_CHAR yy_meta[4] =
     {   0,
-        1,    1,    1,    2
+        1,    2,    2
     } ;
 
-static const flex_int16_t yy_base[11] =
+static const flex_int16_t yy_base[10] =
     {   0,
-        0,    0,    6,    7,    7,    7,    0,    0,    7,    3
+        0,    0,    5,    0,    6,    6,    0,    6,    3
     } ;
 
-static const flex_int16_t yy_def[11] =
+static const flex_int16_t yy_def[10] =
     {   0,
-        9,    1,    9,    9,    9,    9,   10,   10,    0,    9
+        8,    1,    8,    9,    8,    8,    9,    0,    8
     } ;
 
-static const flex_int16_t yy_nxt[12] =
+static const flex_int16_t yy_nxt[10] =
     {   0,
-        4,    5,    6,    7,    8,    9,    3,    9,    9,    9,
-        9
+        4,    5,    6,    7,    8,    3,    8,    8,    8
     } ;
 
-static const flex_int16_t yy_chk[12] =
+static const flex_int16_t yy_chk[10] =
     {   0,
-        1,    1,    1,    1,   10,    3,    9,    9,    9,    9,
-        9
+        1,    1,    1,    9,    3,    8,    8,    8,    8
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -438,8 +436,12 @@ int yy_flex_debug = 0;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "hide-digits.l"
-#line 443 "lex.yy.c"
+#line 1 "word-spliter.l"
+#line 2 "word-spliter.l"
+#define T_WORD 1
+int numChars = 0, numWords = 0, numLines = 0;
+#line 444 "lex.yy.c"
+#line 445 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -656,9 +658,9 @@ YY_DECL
 		}
 
 	{
-#line 1 "hide-digits.l"
+#line 8 "word-spliter.l"
 
-#line 662 "lex.yy.c"
+#line 664 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -685,13 +687,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 10 )
+				if ( yy_current_state >= 9 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 7 );
+		while ( yy_base[yy_current_state] != 6 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -716,28 +718,31 @@ do_action:	/* This label is used only to access EOF actions. */
 			goto yy_find_action;
 
 case 1:
+/* rule 1 can match eol */
 YY_RULE_SETUP
-#line 2 "hide-digits.l"
-printf("?");
+#line 9 "word-spliter.l"
+{ numLines++; numChars++; }
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 3 "hide-digits.l"
-return 0;
+#line 10 "word-spliter.l"
+{ numWords++; numChars += yyleng; return T_WORD; }
+	YY_BREAK
+case YY_STATE_EOF(INITIAL):
+#line 11 "word-spliter.l"
+{ return 0; }
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 4 "hide-digits.l"
-ECHO;
+#line 12 "word-spliter.l"
+{ numChars++; }
 	YY_BREAK
 case 4:
 YY_RULE_SETUP
-#line 5 "hide-digits.l"
+#line 13 "word-spliter.l"
 ECHO;
 	YY_BREAK
-#line 739 "lex.yy.c"
-case YY_STATE_EOF(INITIAL):
-	yyterminate();
+#line 746 "lex.yy.c"
 
 	case YY_END_OF_BUFFER:
 		{
@@ -1032,7 +1037,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 10 )
+			if ( yy_current_state >= 9 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1060,11 +1065,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 10 )
+		if ( yy_current_state >= 9 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 9);
+	yy_is_jam = (yy_current_state == 8);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1740,11 +1745,16 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 5 "hide-digits.l"
+#line 13 "word-spliter.l"
 
 
 int main(int argc, char* argv[]) {
-    yylex();
+    int token_type;
+    while (token_type = yylex()) {
+        printf("WORD:\t%s\n", yytext);
+    }
+    printf("\nChars\tWords\tLines\n");
+    printf("%d\t%d\t%d\n", numChars, numWords, numLines);
     return 0;
 }
 
